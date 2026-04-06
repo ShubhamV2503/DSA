e1c191e7b4f421563e055b158c39b16f7184b839
Shubham Vishwakarma <65219379+ShubhamV2503@users.noreply.github.com>
2026-04-06 20:35:50 +0530

    Update quick_sort.py

--- Sorting Technique/quick_sort.py
+++ Sorting Technique/quick_sort.py
@@ -1,8 +1,6 @@
 ## QUICK SORT
-
-
+# optimized code
 a = [4,1,7,9,3]
-
 
 def sort(a,low,high):
 
@@ -13,12 +11,11 @@ def sort(a,low,high):
     while i < j:
         
 
-
-
 def quick_sort(a,low,high):
     if low < high:
         partision = sort(a,low,high)
         quick_sort(a,low,partision)
         quick_sort(a,partision,high)
 
-
+#TC:
+#sc:
