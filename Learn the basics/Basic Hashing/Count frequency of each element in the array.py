791bf0ffbc05fcbba29493c4e8cccc5644f3b911
Shubham Vishwakarma <65219379+ShubhamV2503@users.noreply.github.com>
2026-04-09 17:23:47 +0530

    Update Count frequency of each element in the array.py

--- Learn the basics/Basic Hashing/Count frequency of each element in the array.py
+++ Learn the basics/Basic Hashing/Count frequency of each element in the array.py
@@ -13,17 +13,17 @@
 
 # ## TC = O(Log(N)) for Ordered Map and SC = O(N)
 
-# a = [3, 1, 2, 3, 2]
+a = [3, 1, 2, 3, 2]
 
-# map = {}
+map = {}
 
-# for i in range(len(a)):
-#     if a[i] not in map:
-#         map[a[i]] = 1
-#     else:
-#         map[a[i]] = map[a[i]] + 1
+for i in range(len(a)):
+    if a[i] not in map:
+        map[a[i]] = 1
+    else:
+        map[a[i]] = map[a[i]] + 1
 
-# print(map)
+print(map)
 
 #--------------------------------------------------------------#
 
